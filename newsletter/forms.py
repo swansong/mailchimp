17711c6e1976017fd7180b6fed9e3fa24f4bb2b3
Jon Swanson <jswan2@uw.edu>
2014-03-14 16:52:20 -0700

    extended content length limit to allow for larger posts

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -37,7 +37,7 @@ class CustomClearableImageInput(ClearableFileInput):
 class NewsItemForm(ModelForm):
     POSITION_CHOICES = [(0, '0 (Top Left)'), (1, '1 (Middle Left)'), (2, '2 (Bottom Left)'), (3, '3 (Top Right)'), (4, '4 (Middle Right)'), (5, '5 (Bottom Right)'), (6, 'Draft (not published)')]
     title = forms.CharField(max_length=96, required=False)
-    content = forms.CharField(max_length=256, widget=TinyMCE(attrs={'cols':60, 'rows':30}), required=True)
+    content = forms.CharField(max_length=512, widget=TinyMCE(attrs={'cols':60, 'rows':30}), required=True)
     date_to_publish = forms.DateField(required=True)
     position = forms.ChoiceField(widget=forms.RadioSelect, choices=POSITION_CHOICES, required=True)
     image = forms.ImageField(widget=CustomClearableImageInput, required=False)
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+#from django.core.files.storage import Storage
 
-# Create your models here.
+#class CustomStorageOverride(FileSystemStorage):
+#    """this is a custom storage class that will prevent duplicate files from being created
+#    """
+#    def get_available_name(self, name):
+#        return name
+#
+#    def _save(self, name, content):
+#        if self.exists(name):
+#            return name
+#        else:
+#            return super(CustomStorageOverride, self)._save(name, content)
+
 
 class NewsItem(models.Model):
     """NewsItems contain all the content and date to publish for the newsletter sections
     """
     title = models.CharField(max_length=96, blank=True)
-    content = models.TextField(max_length=256, blank=True)
+    content = models.TextField(max_length=512, blank=True)
     date_to_publish = models.DateField(null=True)
     create_date = models.DateField(auto_now_add=True)
     position = models.IntegerField(blank=True, null=True, default=0)
